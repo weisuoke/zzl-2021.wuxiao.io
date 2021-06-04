d13cbde1a542b01d88f173ed2329b517531b8dd2
梅林的法杖 <530264127@qq.com>
2021-06-04 14:32:44 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,21 @@
+const config = {
+  mode: 'site',
+  title: '前端早早聊 - 2021',
+  description: '学习笔记',
+  publicPath: '/',
+  base: '/',
+  exportStatic: {},
+  dynamicImport: {},
+  theme: {
+    '@primary-color': '#00A7D6',
+  },
+  navs: [
+    null
+  ]
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  config.ssr = {};
+}
+
+export default config;
---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+title: 前端早早聊 - 2021
+order: 10
+hero:
+  title: 前端早早聊 - 2021
+  desc: 📚 笔记 | 思路 | 扩展
+  actions:
+    - text: 开始阅读
+      link: /
+footer: Open-source MIT Licensed | Copyright © 2021-present<br />Powered by wuxiao.io
+---
